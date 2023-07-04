f5d1169656190f6faa71c6ee3bfbd40681835487
Jalen <101148376+JalenWTH@users.noreply.github.com>
2023-07-03 21:22:03 -0400

    Insert better filler text into videoCard component properties

--- Recommended.jsx
+++ Recommended.jsx
@@ -5,14 +5,78 @@ import './Recommended.css';
 export default function Recommended() {
   return (
     <div className='recommended'>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
-        <VideoCard thumbnail='https://source.unsplash.com/random' title='This is a Youtube Video' views='1m' channel_name='JalenWithTheHead' timestamp='6/27/2023'/>
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/flower' 
+          title='This is a Youtube Video' 
+          views='1m'
+          channelName='JalenWithTheHead' 
+          timestamp='6/27/2023'
+          channelImage='https://iili.io/HiULotj.png'
+        />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/r' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channelName='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+       
+         <VideoCard 
+          thumbnail='https://source.unsplash.com/featured/a' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channelName='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/b' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channelName='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/c' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channel_name='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/d' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channel_name='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/g' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channel_name='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random/f' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channel_name='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
+
+        <VideoCard 
+          thumbnail='https://source.unsplash.com/random' 
+          title='This is a Youtube Video' 
+          views='1m' 
+          channel_name='JalenWithTheHead'
+          timestamp='6/27/2023'
+         />
     </div>
   )
 }
