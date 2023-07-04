a3e133cd91f5fd973b0a75047edc06005d3db36d
Jalen <101148376+JalenWTH@users.noreply.github.com>
2023-07-03 21:23:45 -0400

    Update ChannelRow.jsx

--- ChannelRow.jsx
+++ ChannelRow.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './ChannelRow.css';
+import VerifiedIcon from '../../assets/Icons/VerifiedIcon.jsx';
+
 
 function ChannelRow({
-    image,
+    imageSource,
     channel,
     subscribers,
     videoCount,
@@ -14,11 +16,14 @@ function ChannelRow({
       <img
         className='channelRow__image'
         alt='channel image'
-        src={image}
+        src={imageSource}
       />
 
       <div className='channelRow__text'>
-        <h4>{channel}</h4>
+        <h4>{channel} {verified && <VerifiedIcon/>} </h4>
+        <p>{subscribers} Subscribers</p>
+        <p>{videoCount} Videos</p>
+        <p>{description}</p>
       </div>
     </div>
   )
