5b15bd966398239d0094c49cc5696760b2a35548
Jalen <101148376+JalenWTH@users.noreply.github.com>
2023-07-03 21:24:41 -0400

    Update VideoRow.jsx

--- VideoRow.jsx
+++ VideoRow.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './VideoRow.css';
 
-function VideoRow() {
+function VideoRow({
+  imageSource,
+  channel,
+  subscribers,
+  description,
+  timeStamp,
+  views,
+  title
+}) {
   return (
-    <div>
-      VideoRow
+    <div className='videoRow'>
+        <img src={imageSource} alt='videoRow__image'/>
+        <div className='videoRow__text'>
+            <h4>{title}</h4>
+        </div>
+            <p>{views} · {timeStamp} </p>
+
+            <p>{description}</p>
     </div>
   )
 }
